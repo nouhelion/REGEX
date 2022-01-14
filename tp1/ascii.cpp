207a126621cdb3490644752d89c837a7cab18525
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-14 20:57:40 +0100

    change

---
+++ tp1/ascii.cpp
@@ -0,0 +1,3 @@
+//Using a type declaration
+//union, write a CTOA function, which reads a
+//character and returns the corresponding ascii code.
--- tp1/parenthese.cpp
+++ tp1/parenthese.cpp
@@ -1,34 +1,38 @@
-//what we supposed to do is below here
-//Write a program that checks that the
-//parentheses of a string, typed on the keyboard, are
-//well balanced.
-// The program should display one of 3 messages:
-// Parentheses are well balanced
-// Missing, 'n' opening parentheses
-// missing, 'n' closing parentheses 
+// what we supposed to do is below here
+// Write a program that checks that the
+// parentheses of a string, typed on the keyboard, are
+// well balanced.
+//  The program should display one of 3 messages:
+//  Parentheses are well balanced
+//  Missing, 'n' opening parentheses
+//  missing, 'n' closing parentheses
 #include <cstring>
-#include <stdlib.h>
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 void parentheses(string chaine)
 {
-   int i=0,j=0;
-   while(i<chaine.length())
-   {
-       if(chaine[i]=='(')
-             ++j;
-       else if(chaine[i]==')')
-             --j; 
+    int i = 0, j = 0;
+    while (i < chaine.length())
+    {
+        if (chaine[i] == '(')
+            ++j;
+        else if (chaine[i] == ')')
+            --j;
         i++;
-   }
-   if(!j) cout<<"Parentheses are well balanced"<<endl;
-   else if(j<0) cout<<"missing, "<<-j<<" opening parentheses "<<endl;
-   else  cout<<"missing, "<<j<<" closing parentheses "<<endl;
+    }
+    if (!j)
+        cout << "Parentheses are well balanced" << endl;
+    else if (j < 0)
+        cout << "missing, " << -j << " opening parentheses " << endl;
+    else
+        cout << "missing, " << j << " closing parentheses " << endl;
 }
- int main(int argc, char** argv){
-     string chaine;
-     cout<<"Enter the phrase :"<<endl;
-     getline(cin,chaine);
-     parentheses(chaine);
-     return 0;
- }
+int main(int argc, char **argv)
+{
+    string chaine;
+    cout << "Enter the phrase :" << endl;
+    getline(cin, chaine);
+    parentheses(chaine);
+    return 0;
+}
