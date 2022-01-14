461e9ff84cf5eb91ec1ddf77159c6fffaf5a81f0
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-14 22:46:12 +0100

    Create syntaxe.cpp

---
+++ tp1/syntaxe.cpp
@@ -0,0 +1,5 @@
+//Write a program that has the effect of synthesizing
+//arithmetic expressions composed of the single digit 4,
+//four operators +, -, *, / and parentheses (,).
+//For example: we can write: "2 = (4+4)/4" and "5=4+(4/4)".
+//We test this program for the integers from 1 to 19.
