37b31c3259df76659ea66fda52f14ea8a206ffdd
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-14 21:10:40 +0100

    ascii

--- tp1/ascii.cpp
+++ tp1/ascii.cpp
@@ -1,3 +1,25 @@
 //Using a type declaration
 //union, write a CTOA function, which reads a
 //character and returns the corresponding ascii code.
+#include <cstring>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+//structure ???? 
+union {
+    char c;
+    unsigned i;
+}p;
+void CTOA(){
+    p.i=0;
+    cout << "Enter the phrase :" << endl;
+    cin>>p.c;
+    cout<<"ASCII code : "<<p.i<< endl;
+}
+
+int main(int argc, char **argv)
+{
+    CTOA();
+    system("pause");
+    return 0;
+}
