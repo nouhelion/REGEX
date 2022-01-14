ea47a1b745ac43835a120e5e5cff98f2b69a620d
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-14 21:53:56 +0100

    even odd a

--- tp1/odd_even.cpp
+++ tp1/odd_even.cpp
@@ -1,15 +1,57 @@
-//Write a program that implements an automaton that indicates if a string
-//of ‘a’ consists of an even or odd number of ‘a’.
+// Write a program that implements an automaton that indicates if a string
+// of ‘a’ consists of an even or odd number of ‘a’.
 #include <cstring>
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-//structure ???? 
+void number(string chaine)
+{
+    int i = 0, state = 0;
+    while (1)
+    {
+        switch (state)
+        {
+        case 0:
+            {if (chaine[i] == 'a')
+            {
+                state = 1;
+                i++;
+            }
+            else if (i == chaine.length())
+            {
+                cout << "It consists of an even number of a" << endl;
+                exit(0);
+            }
+            else{
+                cout << "It's not < a > " << endl;
+                exit(1);
+            }
+            break;}
+        case 1:
+            {if (chaine[i] == 'a')
+            {
+                state = 0;
+                i++;
+            }
+            else if (i == chaine.length())
+            {
+                cout << "It consists of an odd number of a" << endl;
+                exit(0);
+            }
+            else{
+                cout << "It's not < a > " << endl;
+                exit(1);
+            }
+            break;}
+        }
+    }
+}
 
 int main(int argc, char **argv)
 {
-   string chaine;
+    string chaine;
     cout << "Enter the phrase :" << endl;
     getline(cin, chaine);
+    number(chaine);
     return 0;
 }
