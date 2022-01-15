17fa531abf5ec5ed4eff9f314d57c5529c84c5fa
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-15 12:45:46 +0100

    Create UNZero.cpp

---
+++ Part_B/UNZero.cpp
@@ -0,0 +1,5 @@
+//Write a program that implements positive integers multiple of 10, without zeros
+//unnecessary.
+//Valid integers: "0", "1000", "30050", "1500"
+//Invalid integer: "00340"
+//REGEX 
