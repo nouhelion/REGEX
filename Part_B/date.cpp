fcd6a521d37b02851cacd7bd2d092334491b49a4
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-15 10:14:11 +0100

    Create date.cpp

---
+++ Part_B/date.cpp
@@ -0,0 +1,6 @@
+//We want to recognize a date in the form:
+//03/02 for February 3, limited to the current year.
+//For simplicity, we consider that each month is made up
+//30 days.
+//Write a program that implements this automaton.
+//regex => 
