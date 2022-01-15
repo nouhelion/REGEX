1b3319d8635693a1c31a09026ac194a6ebe9f72e
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-15 21:19:05 +0100

    Create comment_c.cpp

---
+++ Part_B/comment_c.cpp
@@ -0,0 +1,80 @@
+//Write a program that implements a deterministic finite state machine
+//which validates a C-style comment and displays the content of the comment.
+//Example: 
+/* a body of remark */
+//Examples: /***/ /*a**b*/ /**A*/
+
+#include <cstring>
+#include <ctype.h>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+void email(string chaine)
+{
+    string comment;
+    int i = 0, j = 0, state = 0;
+    while (1)
+    {
+        switch (state)
+        {
+        case 0:
+        {
+            if (chaine[i] == '/')
+            {
+                state = 1;
+                i++;
+            }
+            else
+            {
+                cout << "!!! Erreur \n\t ";
+                exit(1);
+            }
+            break;
+        }
+        case 1:
+        {
+            if (chaine[i] == '/')
+            {
+                state = 2;
+                i++;
+            }
+            else
+            {
+                cout << "!!! Erreur ";
+                exit(1);
+            }
+            break;
+        }
+        case 2:
+        {
+            if (chaine[i] == '\n' || i == chaine.size())
+            {
+                state = 3;
+            }
+            else
+            {
+                comment[j] = chaine[i];
+                j++;
+                i++;
+            }
+            break;
+        }
+        case 3:
+        {
+            cout << "Le corps du commentaire est: ";
+            for (int n = 0; n < j; n++)
+                cout << comment[n];
+            // for(i=2; i<chaine.size(); i++) cout<< chaine[i]);
+        }
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    string chaine;
+    cout << "Enter the phrase :" << endl;
+    getline(cin, chaine);
+    email(chaine);
+    return 0;
+}
