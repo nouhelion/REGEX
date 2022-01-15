8c79bc1e6f1817832c94a0980eca2f108911de86
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-15 12:26:17 +0100

    bNUM

---
+++ Part_B/bNUM.cpp
@@ -0,0 +1,100 @@
+//Écrire un programme qui implante cet automate et donne
+//le nombre de ‘b’ dans la chaine.
+//Exemples de mots valides : "aa", "aba", "a"
+//Mots non valides : "abb", "ab"
+//REGEX a(b)*a|a
+
+#include <cstring>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+void bNUM(string chaine)
+{
+    int i = 0, state = 0;
+    while (1)
+    {
+        switch (state)
+        {
+        case 0:
+        {
+            if (chaine[i] == 'a')
+            {
+                state = 1;
+                i++;
+            }
+            else
+            {
+                cout << "erreur" << endl;
+                exit(1);
+            }
+            break;
+        }
+        case 1:
+        {
+            if (i==chaine.length())
+            {
+                cout << "valide" << endl;
+                exit(0);
+            }
+            else if (chaine[i] == 'a')
+            {
+                 state = 3;
+                i++;
+            }
+             else if (chaine[i] == 'b')
+            {
+                 state = 2;
+                 i++;
+            }
+            else
+            {
+                cout << "erreur" << endl;
+                exit(1);
+            }
+            break;
+        }
+        case 2:
+        {
+           if (chaine[i] == 'a')
+            {
+                 state = 3;
+                i++;
+            }
+             else if (chaine[i] == 'b')
+            {
+                 state = 2;
+                 i++;
+            }
+            else
+            {
+                cout << "erreur" << endl;
+                exit(1);
+            }
+            break;
+        }
+        case 3:
+        {
+             if (i==chaine.length())
+            {
+                cout << "valide" << endl;
+                exit(0);
+            }
+            else
+            {
+                cout << "erreur" << endl;
+                exit(1);
+            }
+            break;
+        }
+        }
+    }
+}
+
+int main(int argc, char **argv)
+{
+    string chaine;
+    cout << "Enter the phrase :" << endl;
+    getline(cin, chaine);
+    bNUM(chaine);
+    return 0;
+}
