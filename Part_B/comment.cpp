8233688b87aacdf8cae92e8abc0322e8e26c72f6
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-15 16:41:47 +0100

    Create comment.cpp

---
+++ Part_B/comment.cpp
@@ -0,0 +1,5 @@
+//Write a program that implements a deterministic finite state machine
+//which validates a C++-like comment and displays the contents of the
+//remark.
+//Example: // a comment body \n
+
