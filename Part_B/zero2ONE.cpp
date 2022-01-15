43ebec76808bd0ccd542fe7b6e58f13955e4c9b8
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-15 13:51:49 +0100

    Create zero2ONE.cpp

---
+++ Part_B/zero2ONE.cpp
@@ -0,0 +1,5 @@
+//Write a program that validates words recognized 
+//by the alphabet {0, 1}
+//of words that contain the string "011".
+//Valid examples: "011", "10101100", "110101100", "01011"
+//REGEX 
