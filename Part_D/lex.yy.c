1fc28f7896312ae57a3ec9964044dd5bad9b59bb
yare-yare <Etu_KAMEL.Nouhaila@emsi-edu.ma>
2022-01-17 02:00:49 +0100

    line word

--- Part_D/lex.yy.c
+++ Part_D/lex.yy.c
@@ -360,9 +360,9 @@ struct yy_trans_info
 	flex_int32_t yy_verify;
 	flex_int32_t yy_nxt;
 	};
-static const flex_int16_t yy_accept[9] =
+static const flex_int16_t yy_accept[8] =
     {   0,
-        0,    0,    4,    1,    3,    2,    1,    0
+        0,    0,    4,    3,    1,    2,    0
     } ;
 
 static const YY_CHAR yy_ec[256] =
@@ -376,7 +376,7 @@ static const YY_CHAR yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    2,    1,    1,    1,    1,    1,    1,
 
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -399,27 +399,27 @@ static const YY_CHAR yy_ec[256] =
 
 static const YY_CHAR yy_meta[4] =
     {   0,
-        1,    2,    2
+        1,    1,    1
     } ;
 
-static const flex_int16_t yy_base[10] =
+static const flex_int16_t yy_base[8] =
     {   0,
-        0,    0,    5,    0,    6,    6,    0,    6,    3
+        0,    0,    4,    5,    5,    5,    5
     } ;
 
-static const flex_int16_t yy_def[10] =
+static const flex_int16_t yy_def[8] =
     {   0,
-        8,    1,    8,    9,    8,    8,    9,    0,    8
+        7,    1,    7,    7,    7,    7,    0
     } ;
 
-static const flex_int16_t yy_nxt[10] =
+static const flex_int16_t yy_nxt[9] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8
+        4,    5,    6,    7,    3,    7,    7,    7
     } ;
 
-static const flex_int16_t yy_chk[10] =
+static const flex_int16_t yy_chk[9] =
     {   0,
-        1,    1,    1,    9,    3,    8,    8,    8,    8
+        1,    1,    1,    3,    7,    7,    7,    7
     } ;
 
 static yy_state_type yy_last_accepting_state;
@@ -436,12 +436,13 @@ int yy_flex_debug = 0;
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "numWORD.l"
-#line 2 "numWORD.l"
+#line 1 "line_word.l"
+#line 2 "line_word.l"
     #include <stdio.h>
-    int counter=0;
-#line 443 "lex.yy.c"
+    int lines=1;
+    int characters=0;
 #line 444 "lex.yy.c"
+#line 445 "lex.yy.c"
 
 #define INITIAL 0
 
@@ -658,9 +659,9 @@ YY_DECL
 		}
 
 	{
-#line 5 "numWORD.l"
+#line 6 "line_word.l"
 
-#line 663 "lex.yy.c"
+#line 664 "lex.yy.c"
 
 	while ( /*CONSTCOND*/1 )		/* loops until end-of-file is reached */
 		{
@@ -687,13 +688,13 @@ yy_match:
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 8 )
 					yy_c = yy_meta[yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 5 );
 
 yy_find_action:
 		yy_act = yy_accept[yy_current_state];
@@ -719,19 +720,18 @@ do_action:	/* This label is used only to access EOF actions. */
 
 case 1:
 YY_RULE_SETUP
-#line 6 "numWORD.l"
-{counter++;}
+#line 7 "line_word.l"
+{characters++;}
 	YY_BREAK
 case 2:
 /* rule 2 can match eol */
 YY_RULE_SETUP
-#line 7 "numWORD.l"
-{printf("%d words exist",counter);
-exit(0);}
+#line 8 "line_word.l"
+{lines++;}
 	YY_BREAK
 case 3:
 YY_RULE_SETUP
-#line 9 "numWORD.l"
+#line 9 "line_word.l"
 ECHO;
 	YY_BREAK
 #line 737 "lex.yy.c"
@@ -1031,7 +1031,7 @@ static int yy_get_next_buffer (void)
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 8 )
 				yy_c = yy_meta[yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
@@ -1059,11 +1059,11 @@ static int yy_get_next_buffer (void)
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 8 )
 			yy_c = yy_meta[yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 7);
 
 		return yy_is_jam ? 0 : yy_current_state;
 }
@@ -1739,13 +1739,21 @@ void yyfree (void * ptr )
 
 #define YYTABLES_NAME "yytables"
 
-#line 9 "numWORD.l"
+#line 9 "line_word.l"
 
- int main(){
-     printf("enter a string");
-     yylex();
-     return 0;
- }
- int yywrap(){
-     return 1;
- }
+int main() {
+    FILE *file;
+    char name[100];
+    printf("enter name of file : ");
+    gets(name);
+    file=fopen(name, "r+");
+    yyin=file;
+    yylex();
+    fclose(file);
+    printf("\nnumber of characters : %d\n",characters);
+    printf("\nnumber of lines : %d\n",lines);
+	return 0;
+}
+int yywrap(){
+    return 1;
+}
